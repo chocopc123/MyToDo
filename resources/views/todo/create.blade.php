eef5329aaf7379f86a43028786aabb954edec767
choco <0215ryuuku@gmail.com>
2020-07-11 17:18:06 +0900

    "create view"スタイル変更

--- resources/views/todo/create.blade.php
+++ resources/views/todo/create.blade.php
@@ -2,33 +2,37 @@
 <html lang="ja">
   <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <title>ToDo追加|MyToDo</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css"><!-- Bootstrap本体 -->
   </head>
-  <body class="px-3">
+  <body class="p-3">
     <h3>ToDo追加</h3>
 
     <form method="POST" action="/create">
       {{ csrf_field() }}
       <div class="form-group">
-        <label>タイトル<input type="text" class="form-control" name="title"></label>
+        <label for="titleInput">タイトル</label>
+        <input type="text" class="form-control" id="titleInput" name="title">
       </div>
       <div class="form-group">
-        <label>詳細<textarea class="form-control" name="explanation" cols="30" rows="10"></textarea></label>
+        <label for="explanationInput">詳細</label>
+        <textarea class="form-control" id="explanationInput" name="explanation" cols="30" rows="10"></textarea>
       </div>
       <div class="form-group">
-        <label>難易度<input type="range" name="difficulty" min="1" max="3" value="1"></label>
+        <label>難易度<input type="range" class="form-control-range" name="difficulty" min="1" max="3" value="1"></label>
       </div>
       <div class="form-group">
-        <label>重要度<input type="range" name="importance" min="1" max="3" value="1"></label>
+        <label>重要度<input type="range" class="form-control-range" name="importance" min="1" max="3" value="1"></label>
       </div>
       <div class="form-group">
         <label>目標期限<input type="date" class="form-control" name="deadline"></label>
       </div>
-        <input type="submit" class="btn btn-primary" value="追加"></li>
+      <input type="submit" class="btn btn-primary" value="追加"></li>
     </form>
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script><!-- Scripts（Jquery） -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script><!-- Scripts（bootstrapのjavascript） -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
   </body>
 </html>
